0893bfabdf491dac65d85a6ce625f1de5e971816
Fabio Jordão <fabiosjordao@gmail.com>
2021-05-07 18:06:39 -0300

    Doing: aula05 starting chai

--- modulo01/aula05-tdd-project-part02/src/service/carService.js
+++ modulo01/aula05-tdd-project-part02/src/service/carService.js
@@ -5,8 +5,15 @@ class CarService {
         this.carRepository = new BaseRepository({ file: cars})
     }
 
-    test(id) {
-        return this.carRepository.find(id)
+    getRandomPositionFromArray(list) {
+        const listLength = list.length;
+        return Math.floor(
+            Math.random() * (listLength)
+        )
+    }
+    
+    async getAvailableCar(carCategory) {
+        return {}
     }
 }
 
--- modulo01/aula05-tdd-project-part02/test/unitTests/carService.test.js
+++ modulo01/aula05-tdd-project-part02/test/unitTests/carService.test.js
@@ -2,8 +2,15 @@ const { describe, it, before } = require('mocha');
 const CarService = require('./../../src/service/carService');
 
 const { join } = require('path');
+const { expect } = require('chai');
 
-const carsDatabase = join(__dirname, './../../../database', 'cars.json');
+const carsDatabase = join(__dirname, './../../database', 'cars.json');
+
+const mocks = {
+    validCarCategory: require('./../mocks/valid-carCategory.json'),
+    validCar: require('./../mocks/valid-car.json'),
+    validCustomer: require('./../mocks/valid-customer.json')
+}
 
 describe('Carservice Suite Tests', () => {
     let carService = {}
@@ -13,8 +20,30 @@ describe('Carservice Suite Tests', () => {
         });
     })
 
-    it('given a carCategory it should return an available car', async () => {
-        const result = await carService.test();
-        console.log('result', result);
+    it('shoul retrieve a random position from an array', () => {
+        const data = [0, 1, 2, 3, 4];
+        const result = carService.getRandomPositionFromArray(data);
+
+        expect(result).to.be.lte(data.length).and.be.gte(0);
     })
+
+    it('should choose the first id from carIds in carCategory', () => {
+        const carCategory = mocks.validCarCategory
+        const carIdIndex =  0;
+
+        const result = carService.chooseRandomCar(carCategory);
+        const expected = carCategory.carIds(carIdIndex);
+
+        expect(result).to.be.equal(expected);
+    })
+    // it('given a carCategory it should return an available car', async () => {
+    //     const car = mocks.validCar;
+    //     const carCategory = Object.create(mocks.validCarCategory);
+    //     carCategory.ids = [car.id];
+
+    //     const result = await carService.getAvailableCar(carCategory);
+    //     const expected = car;
+
+    //     expect(result).to.be.deep.equal(expected);
+    // })
 })
